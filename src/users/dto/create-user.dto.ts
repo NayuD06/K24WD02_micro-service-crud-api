27d26e80ed4b8694cb1dde3f42577ed9a6e47126
Duy An <anclashofclanshall13@gmail.com>
2026-03-24 19:00:12 +0700

    update3

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import { Transform, Type } from 'class-transformer';
+import {
+  IsString,
+  IsEmail,
+  Length,
+  IsPhoneNumber,
+  IsInt,
+  Min,
+  Max,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(5, 50)
+  readonly name: string;
+
+  @IsEmail()
+  readonly email: string;
+
+  @Transform(({ value }) => value.replace(/\s+/g, '')) // Loại bỏ tất cả ký tự không phải số
+  @IsPhoneNumber('VN')
+  readonly phone: string;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(18, { message: 'Age must be at least 18' })
+  @Max(65, { message: 'Age must be at most 65' })
+  readonly age: number;
+}
