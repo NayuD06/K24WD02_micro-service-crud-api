dd6375e3320b30fd9709ca352b75b91aa93a54bd
Duy An <anclashofclanshall13@gmail.com>
2026-03-24 20:36:47 +0700

    update category

--- src/menu/dto/create-menu.dto.ts
+++ src/menu/dto/create-menu.dto.ts
@@ -1,1 +1,13 @@
-export class CreateMenuDto {}
+import { IsMongoId, IsNumber, IsString, Length, Max, Min } from 'class-validator';
+
+export class CreateMenuDto {
+  @IsString()
+  @Length(2, 50)
+  readonly name: string;
+  @IsNumber()
+  @Min(0)
+  @Max(100, { message: 'Please input less than $100' })
+  readonly price: number;
+  @IsMongoId()
+  readonly category: string;
+}
